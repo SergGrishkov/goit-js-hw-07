2006003bb2121e43cc5b00bc0a4d7a606396bed1
SergGrishkov <serg.grishkov@gmail.com>
2023-12-02 19:49:31 +0200

    Added task 4

---
+++ js/task-04.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form.login-form');
+const email = form.querySelector('input[type=email]');
+const password = form.querySelector('input[type=password]');
+console.log(email);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!event.target[0].value || !event.target[1].value) {
+    alert('All form fields must be filled in');
+  } else {
+    let emName = event.target.firstElementChild.textContent.trim();
+    let emValue = event.target[0].value.trim();
+    let pwdName = event.target.lastElementChild.textContent.trim();
+    let pwdValue = event.target[1].value.trim();
+    const result = {
+      [emName]: emValue,
+      [pwdName]: pwdValue,
+    };
+    console.log(result);
+    form.reset();
+  }
+});
