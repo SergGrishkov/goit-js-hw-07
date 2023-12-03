af869b42839494daef8edb2b0d8847a8552f6b5b
SergGrishkov <serg.grishkov@gmail.com>
2023-12-03 15:39:08 +0200

    Added task 5

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const body = document.body;
+const button = body.querySelector('button.change-color');
+const span = body.querySelector('span.color');
+
+button.addEventListener('click', event => {
+  console.log(event);
+  let rgbColor = getRandomHexColor();
+  body.style.backgroundColor = rgbColor;
+  span.textContent = rgbColor;
+});
